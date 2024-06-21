b04097a8fdc8edf714a9419e01a51ff9218f9efd
KZ <zengke940420@gmail.com>
2024-06-20 22:20:12 -0400

    for wgs sample names.

--- Scripts/Normalization.R
+++ Scripts/Normalization.R
@@ -31,7 +31,7 @@ all_files<-list.files("CountsTables/WGSRaw", pattern = "*.txt",full.names = TRUE
 
 for (file in all_files) {
 
-  countsT<-read.table(file, sep = "\t", header = T, row.names = 1)
+  countsT<-read.table(file, sep = "\t", header = T, row.names = 1, check.names = F)
   normT<-normFun(countsT)
 
   write.table(normT, paste0("CountsTables/WGSNorm/", basename(file)), sep = "\t", row.names = T)
